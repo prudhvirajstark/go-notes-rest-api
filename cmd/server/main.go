dac7c75c0c440a47c313125521fdd76e5c938882
prudhvirajstark <panisettiprudhviraj@gmail.com>
2024-02-18 21:09:17 +0100

    sample start for router

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	fmt.Println("Server running on port ", port)
+
+	if err := http.ListenAndServe(":"+port, router); err != nil {
+		fmt.Println("Failed to start server : ", err)
+	}
 }
